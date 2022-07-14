2e6c7b6fab4a1a533c08b7476ac020f51f154b30
Igor <iigormazetti@hotmail.com>
2022-07-14 20:51:40 -0300

    feat: :white_check_mark: Requisito 10
    Implementado model de BlogPosts com as devidas associaçoes

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,27 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    timestamps: false,
+    tableName: 'BlogPosts',
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'id',
+    });
+  }
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
